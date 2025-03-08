cd149232e4da770a4d8201423e644fe94fe38def
Bogdan Khorolsky <krati18012012@gmail.com>
2025-03-08 09:27:48 -0800

    Qr-Code generator

---
+++ QrCodeGenerator/Generator.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace QrCodeGenerator
+{
+    partial class Generator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Generator));
+            pictureBoxQRCode = new PictureBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SaveQRCode = new Button();
+            menuStrip1 = new MenuStrip();
+            themeToolStripMenuItem = new ToolStripMenuItem();
+            whiteToolStripMenuItem = new ToolStripMenuItem();
+            blackToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            quitToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxQRCode).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBoxQRCode
+            // 
+            pictureBoxQRCode.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBoxQRCode.Image = Properties.Resources.TG_QR_CODE;
+            pictureBoxQRCode.Location = new Point(12, 56);
+            pictureBoxQRCode.Name = "pictureBoxQRCode";
+            pictureBoxQRCode.Size = new Size(617, 234);
+            pictureBoxQRCode.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxQRCode.TabIndex = 0;
+            pictureBoxQRCode.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(370, 65);
+            button1.TabIndex = 1;
+            button1.Text = "Generate Qr Code";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(12, 296);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Qr Code Text";
+            textBox1.Size = new Size(617, 34);
+            textBox1.TabIndex = 2;
+            // 
+            // SaveQRCode
+            // 
+            SaveQRCode.Anchor = AnchorStyles.Bottom;
+            SaveQRCode.Cursor = Cursors.Hand;
+            SaveQRCode.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SaveQRCode.Location = new Point(388, 336);
+            SaveQRCode.Name = "SaveQRCode";
+            SaveQRCode.Size = new Size(241, 65);
+            SaveQRCode.TabIndex = 3;
+            SaveQRCode.Text = "Save Qr Code";
+            SaveQRCode.UseVisualStyleBackColor = true;
+            SaveQRCode.Click += buttonSave_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { themeToolStripMenuItem, aboutToolStripMenuItem, quitToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(641, 24);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // themeToolStripMenuItem
+            // 
+            themeToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { whiteToolStripMenuItem, blackToolStripMenuItem });
+            themeToolStripMenuItem.Name = "themeToolStripMenuItem";
+            themeToolStripMenuItem.Size = new Size(56, 20);
+            themeToolStripMenuItem.Text = "Theme";
+            // 
+            // whiteToolStripMenuItem
+            // 
+            whiteToolStripMenuItem.Name = "whiteToolStripMenuItem";
+            whiteToolStripMenuItem.Size = new Size(105, 22);
+            whiteToolStripMenuItem.Text = "White";
+            whiteToolStripMenuItem.Click += whiteToolStripMenuItem_Click;
+            // 
+            // blackToolStripMenuItem
+            // 
+            blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            blackToolStripMenuItem.Size = new Size(105, 22);
+            blackToolStripMenuItem.Text = "Black";
+            blackToolStripMenuItem.Click += blackToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // quitToolStripMenuItem
+            // 
+            quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            quitToolStripMenuItem.Size = new Size(42, 20);
+            quitToolStripMenuItem.Text = "Quit";
+            quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
+            // 
+            // Generator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(641, 413);
+            Controls.Add(SaveQRCode);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(pictureBoxQRCode);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            Name = "Generator";
+            Text = "Qr-Code Generator";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxQRCode).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxQRCode;
+        private Button button1;
+        private TextBox textBox1;
+        private Button SaveQRCode;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem themeToolStripMenuItem;
+        private ToolStripMenuItem whiteToolStripMenuItem;
+        private ToolStripMenuItem blackToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
---
+++ QrCodeGenerator/Generator.cs
@@ -0,0 +1,88 @@
+﻿namespace QrCodeGenerator
+{
+    public partial class Generator : Form
+    {
+        public Generator()
+        {
+            InitializeComponent();
+        }
+
+        //Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator QRgen = new QRCoder.QRCodeGenerator();
+            var QRdata = QRgen.CreateQrCode(textBox1.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var QRcode = new QRCoder.QRCode(QRdata);
+
+            pictureBoxQRCode.Image = QRcode.GetGraphic(50);
+        }
+        //Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  Generate QR-CODE  
+
+
+        //SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE 
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Проверяем, есть ли изображение в PictureBox
+            if (pictureBoxQRCode.Image == null)
+            {
+                MessageBox.Show("There is no image to save!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Настройка диалога сохранения файла
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                // Настройки диалога
+                saveFileDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|BMP Image|*.bmp"; // Фильтр форматов
+                saveFileDialog.Title = "Save QR code"; // Заголовок диалога
+                saveFileDialog.FileName = "QRCode"; // Имя файла по умолчанию
+
+                // Если пользователь выбрал файл и нажал "Сохранить"
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // Сохраняем изображение в выбранный файл
+                        pictureBoxQRCode.Image.Save(saveFileDialog.FileName);
+                        MessageBox.Show("Image saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error while saving: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        //SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE SAVE QR-CODE 
+
+        //Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit 
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit Quit 
+
+        //White White White White White White White White White White White White White White White White White 
+        private void whiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.WhiteSmoke;
+        }
+        //White White White White White White White White White White White White White White White White White 
+
+        //Black Black Black Black Black Black Black Black Black Black Black Black Black Black Black Black Black 
+        private void blackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+        }
+        //Black Black Black Black Black Black Black Black Black Black Black Black Black Black Black Black Black 
+
+        //ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT 
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version 1.0", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        //ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT 
+
+
+    }
+}
